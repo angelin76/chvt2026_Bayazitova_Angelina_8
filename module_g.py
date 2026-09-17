741b1c2f9cb16b2939f50c5e5f41dcc8e2f137d2
angelin76 <inga.bayazitova43@gmail.com>
2026-09-17 09:43:31 +0300

    Update module_g.py

--- module_g.py
+++ module_g.py
@@ -1,12 +1,14 @@
-
 from tkinter import *
-
  
-root = Tk()     # создаем корневой объект - окно
-root.title("Графическое окно ")     # устанавливаем заголовок окна
-root.geometry("300x250")    # устанавливаем размеры окна
+root = Tk()
+root.title("webots_test_edited")
+root.geometry("250x150") 
  
-label = Label(text="webots_test_edited") # создаем текстовую метку
-label.pack()    # размещаем метку в окне
+main_menu = Menu()
+main_menu.add_cascade(label="Карта")
+main_menu.add_cascade(label="Роботы и точки")
+main_menu.add_cascade(label="Терминал")
+main_menu.add_cascade(label="Параметры Nav2")
  
+root.config(menu=main_menu)
 root.mainloop()
